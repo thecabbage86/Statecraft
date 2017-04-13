29790b9e27c672e9e66839d42a83a837c9921b6a
Caleb Kapusta <calebk@magenic.net>
2017-04-13 14:26:38 -0700

    add Territory and Unit models and associated enums

---
+++ Statecraft.Common/Models/Territory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statecraft.Common.Models
+{
+    public class Territory
+    {
+        private string name;
+
+        public Territory(string name)
+        {
+            this.name = name;
+            this.OccupyingUnit = null;
+        }
+
+        public Unit OccupyingUnit { get; set; }
+        public string Name { get { return name; }  }
+	    //adjacent units?
+    }
+}
---
+++ Statecraft.Common/Models/Unit.cs
@@ -0,0 +1,25 @@
+﻿using Statecraft.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statecraft.Common.Models
+{
+    public class Unit
+    {
+        private Country owner;
+        private UnitType unitType;
+
+        public Unit(Country owner, UnitType unitType)
+        {
+            this.owner = owner;
+            this.unitType = unitType;
+        }
+
+        public Country Owner { get { return owner; } }
+
+        public UnitType UnitType { get { return unitType; } }
+    }
+}
