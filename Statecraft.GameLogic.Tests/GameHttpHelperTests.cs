3b36b009997edd9c34564813bee2b85a21b3e52a
Caleb Kapusta <calebk@magenic.net>
2017-05-02 08:25:04 -0500

    Add GameLogic test project and test for GameHttpHelper

---
+++ Statecraft.GameLogic.Tests/GameHttpHelperTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Statecraft.GameLogic.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statecraft.GameLogic.Tests
+{
+    [TestClass]
+    public class GameHttpHelperTests
+    {
+        GameHttpHelper httpHelper = new GameHttpHelper();
+
+        //TODO: refactor and improve
+        [TestMethod]
+        public void GameHttpHelperTest()
+        {
+            var games = httpHelper.GetGamesByPlayerId(Guid.NewGuid()).Result;
+
+            Assert.IsNotNull(games);
+        }
+    }
+}
